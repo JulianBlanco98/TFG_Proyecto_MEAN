dd1d9c9673f17dbdd621528fbd7e819b747687dc
JulianBlanco98 <jblancoho@alumnos.unex.es>
2024-07-19 19:22:35 +0200

    arreglo de jornada

--- frontend/src/app/pages/jornada/show-jornada/show-jornada.component.ts
+++ frontend/src/app/pages/jornada/show-jornada/show-jornada.component.ts
@@ -1,50 +1,49 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { Jornada } from 'src/app/models/jornada.model';
 import { CrudJornadaService } from 'src/app/services/crud-jornada.service';
-import { faLongArrowLeft, faLongArrowRight } from '@fortawesome/free-solid-svg-icons';
+import {
+  faLongArrowLeft,
+  faLongArrowRight,
+} from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-show-jornada',
   templateUrl: './show-jornada.component.html',
-  styleUrls: ['./show-jornada.component.css']
+  styleUrls: ['./show-jornada.component.css'],
 })
-export class ShowJornadaComponent implements OnInit{
-
+export class ShowJornadaComponent implements OnInit {
   @Input() numJornada: number;
-  jornada: Jornada
+  jornada: Jornada;
 
-  faLeft = faLongArrowLeft
-  faRight = faLongArrowRight
+  faLeft = faLongArrowLeft;
+  faRight = faLongArrowRight;
 
-  constructor(private crudJornadaService: CrudJornadaService){}
+  constructor(private crudJornadaService: CrudJornadaService) {}
 
   ngOnInit(): void {
-    this.getJornada()
+    this.getJornada();
   }
   getJornada() {
-    this.crudJornadaService.getJornadaByNumero(this.numJornada).subscribe(
-      data => {
-        this.jornada = data
-        console.log(this.jornada);        
+    this.crudJornadaService.getJornadaByNumero(this.numJornada).subscribe({
+      next: (data) => {
+        this.jornada = data;
+        console.log(this.jornada);
       },
-      error => {
-        console.log(error);
-        
-      }
-    )
+      error: (err) => {
+        console.log(err);
+      },
+    });
   }
 
   nuevaJornada(jornadaNueva: number) {
     this.crudJornadaService.getJornadaByNumero(jornadaNueva).subscribe(
-        data => {
-            this.jornada = data;
-            console.log(this.jornada);        
-        },
-        error => {
-            console.log(error);
-        }
-    )
-}
-
-
+      (data) => {
+        this.jornada = data;
+        console.log(this.jornada);
+      },
+      (error) => {
+        console.log(error);
+      }
+    );
+  }
 }
