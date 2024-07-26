407ae02d45d84e20fe34ddbd5acaa03d59c29cd2
JulianBlanco98 <jblancoho@alumnos.unex.es>
2024-07-26 18:37:10 +0200

    select hecho de buscar por posicion

--- frontend/src/app/pages/jugadores/show-jugadores-equipo/show-jugadores-equipo.component.ts
+++ frontend/src/app/pages/jugadores/show-jugadores-equipo/show-jugadores-equipo.component.ts
@@ -9,47 +9,74 @@ import { FormsModule } from '@angular/forms';
 @Component({
   selector: 'app-show-jugadores-equipo',
   templateUrl: './show-jugadores-equipo.component.html',
-  styleUrls: ['./show-jugadores-equipo.component.css']
+  styleUrls: ['./show-jugadores-equipo.component.css'],
 })
-export class ShowJugadoresEquipoComponent implements OnInit{
-
-  faBackWard = faArrowLeft
-  faLupa = faSearch
+export class ShowJugadoresEquipoComponent implements OnInit {
+  faBackWard = faArrowLeft;
+  faLupa = faSearch;
 
   constructor(
     private crudJugadoresService: CrudJugadoresService,
     private router: Router,
-    private activatedRoute: ActivatedRoute 
-  ){}
+    private activatedRoute: ActivatedRoute
+  ) {}
 
-  jugadores: Jugador[]
-  idApi: number
-  entrenador: Entrenador | undefined
-  terminoBusqueda = ''
-  
+  jugadores: Jugador[];
+  idApi: number;
+  entrenador: Entrenador | undefined;
+  terminoBusqueda = '';
+  posicionSeleccionada = 'Todos';
+
   ngOnInit(): void {
-     this.idApi = +this.activatedRoute.snapshot.paramMap.get('idApi');
-     this.crudJugadoresService.getJugadoresByIdApi(this.idApi).subscribe((data: any) => {
-      //console.log(data);
-      console.log(data);
-      
-      this.jugadores = data.jugadores;
-      console.log("Jugadores:",this.jugadores);
-      if(data.entrenador && data.entrenador.length > 0){
-        this.entrenador = data.entrenador[0]
-        console.log("Entrenador:",this.entrenador);
-      }
-      
-      
-     })
-
+    this.idApi = +this.activatedRoute.snapshot.paramMap.get('idApi');
+    this.cargarJugadores(this.idApi);
   }
+
+  cargarJugadores(idApi: number) {
+    this.crudJugadoresService
+      .getJugadoresByIdApi(idApi)
+      .subscribe((data: any) => {
+        //console.log(data);
+        console.log(data);
+        this.jugadores = data.jugadores;
+        console.log('Jugadores:', this.jugadores);
+        if (data.entrenador && data.entrenador.length > 0) {
+          this.entrenador = data.entrenador[0];
+          console.log('Entrenador:', this.entrenador);
+        }
+      });
+  }
+  cargarJugadoresPosicion(idApi: number, tipo: string) {
+    this.crudJugadoresService.getJugadoresByPosicion(idApi, tipo).subscribe((data: any) => {
+      this.jugadores = data.jugadores;
+      if (data.entrenador && data.entrenador.length > 0) {
+        this.entrenador = data.entrenador[0];
+      }
+    });
+  }
+
+  seleccionarPosicion(event: Event): void {
+
+    this.posicionSeleccionada = (event.target as HTMLInputElement).value;
+    console.log("Posicion seleecioanda: ",this.posicionSeleccionada);
+    
+    if(this.posicionSeleccionada === 'Todos'){
+      this.cargarJugadores(this.idApi)
+    }
+    else{
+      this.cargarJugadoresPosicion(this.idApi, this.posicionSeleccionada);
+    }
+  }
+
   get jugadoresFiltrados() {
     if (this.terminoBusqueda === '') {
       return this.jugadores;
     } else {
-      return this.jugadores.filter(jugador => jugador.datos.nombreJugador.toLowerCase().includes(this.terminoBusqueda.toLowerCase()));
+      return this.jugadores.filter((jugador) =>
+        jugador.datos.nombreJugador
+          .toLowerCase()
+          .includes(this.terminoBusqueda.toLowerCase())
+      );
     }
   }
-
 }
