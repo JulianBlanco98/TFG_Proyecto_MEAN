db1bd69b1687aba90629966ecde11c8ba876c02b
JulianBlanco98 <jblancoho@alumnos.unex.es>
2024-08-13 14:52:04 +0200

    mejora del modelo para que solo se creen los campos de las apuestas que se hgan

--- backend/model/PrediccionModel.js
+++ backend/model/PrediccionModel.js
@@ -1,79 +1,88 @@
 import mongoose from "mongoose";
 
 const prediccionSchema = new mongoose.Schema(
-    {
-        idUsuario: {
-            type: mongoose.Schema.Types.ObjectId,
-            ref: "users",
-            required: true,
-        },
-        numeroJornada: {
-            type: Number,
-            required: [true, "Por favor, completa este campo"],
-        },
-        monedaInicial: {
-            type: Number,
-            required: [true, "Por favor, completa este campo"],
-        },
-        ganado: {
-            type: Boolean,
-            default: false,
-        },
-        tipo_1: [
-            {
-                indicePartido: {
-                    type: Number,
-                    required: [true, "Por favor, completa este campo"],
-                },
-                prediGanador: {
-                    type: String,
-                    required: [true, "Por favor, completa este campo"],
-                },
-                cantidad: {
-                    type: Number,
-                    required: [true, "Por favor, completa este campo"],
-                },
-            },
-        ],
-        tipo_2: [
-            {
-                idEquipo: {
-                    type: mongoose.Schema.Types.ObjectId,
-                    ref: "equipos",
-                    required: true,
-                },
-                goles: {
-                    type: Number,
-                    required: [true, "Por favor, completa este campo"],
-                },
-                cantidad: {
-                    type: Number,
-                    required: [true, "Por favor, completa este campo"],
-                },
-            },
-        ],
-        tipo_3: [
-            {
-                idEquipo: {
-                    type: mongoose.Schema.Types.ObjectId,
-                    ref: "equipos",
-                    required: true,
-                },
-                asistencias: {
-                    type: Number,
-                    required: [true, "Por favor, completa este campo"],
-                },
-                cantidad: {
-                    type: Number,
-                    required: [true, "Por favor, completa este campo"],
-                },
-            },
-        ],
+  {
+    idUsuario: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "users",
+      required: true,
     },
-    {
-        timestamps: true,
-        versionKey: false,
-    }
+    numeroJornada: {
+      type: Number,
+      required: [true, "Por favor, completa este campo"],
+    },
+    monedaInicial: {
+      type: Number,
+      required: [true, "Por favor, completa este campo"],
+    },
+    ganado: {
+      type: Boolean,
+      default: false,
+    },
+    tipo_1: {
+      type: [
+        {
+          indicePartido: {
+            type: Number,
+            required: [true, "Por favor, completa este campo"],
+          },
+          prediGanador: {
+            type: String,
+            required: [true, "Por favor, completa este campo"],
+          },
+          cantidad: {
+            type: Number,
+            required: [true, "Por favor, completa este campo"],
+          },
+        },
+      ],
+      default: undefined,
+    },
+    tipo_2: {
+      type: [
+        {
+          idEquipo: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "equipos",
+            required: true,
+          },
+          goles: {
+            type: Number,
+            required: [true, "Por favor, completa este campo"],
+          },
+          cantidad: {
+            type: Number,
+            required: [true, "Por favor, completa este campo"],
+          },
+        },
+      ],
+      default: undefined,
+    },
+    tipo_3: {
+      type: [
+        {
+          idEquipo: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "equipos",
+            required: true,
+          },
+          asistencias: {
+            type: Number,
+            required: [true, "Por favor, completa este campo"],
+          },
+          cantidad: {
+            type: Number,
+            required: [true, "Por favor, completa este campo"],
+          },
+        },
+      ],
+      default: undefined,
+    },
+  },
+  {
+    timestamps: true,
+    versionKey: false,
+  }
 );
 
 export const PrediccionModel = mongoose.model("predicciones", prediccionSchema);
