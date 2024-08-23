228ccaa606f65f090a7d6869901a762822d70f33
JulianBlanco98 <jblancoho@alumnos.unex.es>
2024-08-23 13:13:25 +0200

    funciona AOS bien

--- frontend/src/app/pages/inicio/secciones/secciones.component.ts
+++ frontend/src/app/pages/inicio/secciones/secciones.component.ts
@@ -1,10 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import * as Aos from 'aos';
 
 @Component({
   selector: 'app-secciones',
   templateUrl: './secciones.component.html',
   styleUrls: ['./secciones.component.css']
 })
-export class SeccionesComponent {
+export class SeccionesComponent implements OnInit{
 
+  ngOnInit(): void {
+    Aos.init();
+  }
 }
