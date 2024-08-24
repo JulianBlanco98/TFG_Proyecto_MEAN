3c094b027f7f4bf0b41a49bfd695bffa99b9d956
JulianBlanco98 <jblancoho@alumnos.unex.es>
2024-08-24 11:27:31 +0200

    avance en inicio

--- frontend/src/app/pages/inicio/secciones/secciones.component.ts
+++ frontend/src/app/pages/inicio/secciones/secciones.component.ts
@@ -1,5 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 import * as Aos from 'aos';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import { ModalUsuarioComponent } from '../../../components/modal/modal-usuario/modal-usuario.component';
+import { CrudUsersService } from 'src/app/services/crud-users.service';
+import { AlertifyService } from 'src/app/services/alertify.service';
+import { AuthServiceService } from 'src/app/services/auth-service.service';
+import { Router } from '@angular/router';
+import { EventService } from 'src/app/services/event.service';
 
 @Component({
   selector: 'app-secciones',
@@ -7,6 +14,16 @@ import * as Aos from 'aos';
   styleUrls: ['./secciones.component.css'],
 })
 export class SeccionesComponent implements OnInit {
+
+  constructor(
+    private readonly modalService: NgbModal,
+    private readonly crudUsersService: CrudUsersService,
+    private readonly alertifyService: AlertifyService,
+    public authServiceService: AuthServiceService,
+    private readonly router: Router,
+    private readonly eventService: EventService,
+  ){}
+
   ngOnInit(): void {
     Aos.init({
       // Global settings:
@@ -22,11 +39,67 @@ export class SeccionesComponent implements OnInit {
       // Settings that can be overridden on per-element basis, by `data-aos-*` attributes:
       offset: 120, // offset (in px) from the original trigger point
       delay: 0, // values from 0 to 3000, with step 50ms
-      duration: 1000, // values from 0 to 3000, with step 50ms
-      easing: 'ease', // default easing for AOS animations
+      duration: 800, // values from 0 to 3000, with step 50ms
+      easing: 'ease-out-cubic', // default easing for AOS animations
       once: false, // whether animation should happen only once - while scrolling down
       mirror: false, // whether elements should animate out while scrolling past them
       anchorPlacement: 'top-bottom', // defines which position of the element regarding to window should trigger the animation
+    });
+
+
+
+  }
+
+  openModal(tipo: string) {
+    const modal = this.modalService.open(ModalUsuarioComponent, {
+      centered: true,
+      backdrop: true,
+      size: 'md',
+    });
+
+    //El tipo me dice si es login, registro o editar
+    modal.componentInstance.tipo = tipo;
+
+    modal.result.then(
+      (result) => {
+        const response = result;
+        //console.log(response);
+        let nameService: string;
+        if (tipo === 'registro') {
+          nameService = 'registro';
+        } else if (tipo === 'login') {
+          nameService = 'login';
+        } else {
+          nameService = 'editarUsuario';
+        }
+        this.typeSubmit(nameService, response);
+      },
+      () => {
+        //este es el evento de darle a la x para cerrar
+      }
+    );
+  }
+
+  //Mover esta lógica al crud-user-service.ts y crear un
+  typeSubmit(service: any, response: any) {
+    this.crudUsersService[service](response).subscribe({
+      next: (data: any) => {
+        this.alertifyService.success(data.message);
+        if (service === 'login') {
+          this.authServiceService.setToken(data.token);
+
+          //Si el usuario es el admin, redirijo a la pestaña de usuarios
+          this.authServiceService.getRolUsuarioToken().subscribe((rolUsuario) => {
+            if (rolUsuario === 'admin') {
+              this.router.navigateByUrl('/usuarios');
+            }
+          });
+        }
+      },
+      error: (err: any) => {
+        // console.log("error: ", err);
+        this.alertifyService.error(err.error.message);
+      },
     });
   }
 }
